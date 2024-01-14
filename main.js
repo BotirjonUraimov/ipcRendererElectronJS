1da1bc319376702592c6a50c16219e2de57b46f6
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2024-01-14 17:46:01 +0900

    testing package with native model

--- main.js
+++ main.js
@@ -1,126 +1,177 @@
-// main.js
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+// Use of this source code is governed by an Apache 2.0 license
+// that can be found in the LICENSE file.
 
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { join } = require("path");
-const { exec } = require("child_process");
-const { spawn } = require('child_process');
-const url = require('url');
-const fs = require('fs')
-const { promisify } = require('util');
+'use strict';
 
-const unlinkAsync = promisify(fs.unlink);
-const readdirAsync = promisify(fs.readdir);  // Add this line
+/* global describe, it, before, after */
+const assert = require('assert');
 
-let mainWindow;
-let lastRenderedFramePath = null;
-let pythonProcess; // Declare pythonProcess globally
-const outputImagesPath = join(__dirname, 'output_images');
+let rs2 = require('node-librealsense');
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      preload: join(__dirname, "./preload.js"),
-    },
-  });
-
-  mainWindow.loadFile("index.html");
-
-  mainWindow.on("closed", function () {
-    mainWindow = null;
-  });
-
-  mainWindow.loadURL(url.format({
-    pathname: join(__dirname, './index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
-
-  mainWindow.webContents.openDevTools();
-}
-
-app.on("ready", createWindow);
-
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
-});
-
-app.on("activate", function () {
-  if (mainWindow === null) createWindow();
-});
-
-async function stopPythonScript() {
-  if (pythonProcess) {
-    pythonProcess.kill();
-    console.log("pythonProcess killed");
-
-     // Delete all frames in the output_images directory
-     try {
-      const files = await readdirAsync(outputImagesPath);
-      const unlinkPromises = files.map((file) => unlinkAsync(join(outputImagesPath, file)));
-      await Promise.all(unlinkPromises);
-      console.log('All frames deleted');
-    } catch (err) {
-      console.error(`Error deleting frames: ${err}`);
+let frame;
+let pipeline;
+describe('VideoFrame test', function() {
+  before(function() {
+    pipeline = new rs2.Pipeline();
+    pipeline.start();
+    while (!frame) {
+      const frameset = pipeline.waitForFrames();
+      frame = frameset.at(1);
     }
+  });
 
+  after(function() {
+    pipeline.destroy();
+    rs2.cleanup();
+  });
 
-  }
-}
-
-function startPythonScript() {
-  const pythonScriptPath = join(__dirname, 'jumping.py');
-  pythonProcess = spawn('python', [pythonScriptPath]);
-
-  pythonProcess.stdout.on('data', (data) => {
-    const messages = data.toString().split('\n');
-    messages.forEach((message) => {
-      try {
-        const framesData = JSON.parse(message);
-        console.log('Received frames:', framesData);
-        mainWindow.webContents.send('frames', framesData);
-        console.log('Frames data sent successfully.');
-
-        // Update the last rendered frame path
-        lastRenderedFramePath = framesData.frame_path;
-      } catch (parseError) {
-        if (parseError instanceof SyntaxError) {
-          // Ignore non-JSON log messages
-          console.log('Log:', message);
-        } else {
-          console.error(parseError.message);
-        }
-      }
+  it('Testing constructor', () => {
+    assert.doesNotThrow(() => {
+      new rs2.VideoFrame();
     });
   });
 
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Error executing Python script: ${data}`);
+  it('Testing property isValid', () => {
+    assert.equal(typeof frame.isValid, 'boolean');
   });
 
-  pythonProcess.on('close', (code) => {
-    console.log(`Python script finished with code ${code}`);
-
-    //Remove the last rendered frame from disk
-    // if (lastRenderedFramePath) {
-    //   fs.unlink(lastRenderedFramePath, (err) => {
-    //     if (err) {
-    //       console.error(`Error deleting frame: ${err}`);
-    //     } else {
-    //       console.log(`Frame deleted: ${lastRenderedFramePath}`);
-    //     }
-    //   });
-    // }
+  it('Testing property data', () => {
+    assert(Object.prototype.toString.call(frame.data), '[object Uint16Array]' ||
+      Object.prototype.toString.call(frame.data), '[object Uint8Array]'
+    );
   });
-}
 
-ipcMain.on('stop-python-script', () => {
-  stopPythonScript();
-});
+  it('Testing property width', () => {
+    assert.equal(typeof frame.width, 'number');
+  });
 
-ipcMain.on('start-python-script', () => {
-  startPythonScript();
+  it('Testing property height', () => {
+    assert.equal(typeof frame.height, 'number');
+  });
+
+  it('Testing property frameNumber', () => {
+    assert.equal(typeof frame.frameNumber, 'number');
+  });
+
+  it('Testing property timestamp', () => {
+    assert.equal(typeof frame.timestamp, 'number');
+  });
+
+  it('Testing property streamType', () => {
+    assert.equal(typeof frame.streamType, 'number');
+  });
+
+  it('Testing property dataByteLength', () => {
+    assert.equal(typeof frame.dataByteLength, 'number');
+  });
+
+  it('Testing property strideInBytes', () => {
+    assert.equal(typeof frame.strideInBytes, 'number');
+  });
+
+  it('Testing property bitsPerPixel', () => {
+    assert.equal(typeof frame.bitsPerPixel, 'number');
+  });
+
+  it('Testing property bytesPerPixel', () => {
+    assert.equal(typeof frame.bytesPerPixel, 'number');
+    assert.equal(frame.bytesPerPixel, frame.bitsPerPixel/8);
+  });
+
+  it('Testing property timestampDomain', () => {
+    assert.equal(typeof frame.timestampDomain, 'number');
+  });
+
+  it('Testing method frameMetadata - 0 argument', () => {
+    assert.throws(() => {
+      frame.frameMetadata();
+    });
+  });
+
+  it('Testing method frameMetadata - invalid argument', () => {
+    assert.throws(() => {
+      frame.frameMetadata('dummy');
+    });
+  });
+
+  it('Testing method frameMetadata - valid argument', () => {
+    for (let i in rs2.frame_metadata) {
+      if (rs2.frame_metadata[i] &&
+      i.toUpperCase() !== 'FRAME_METADATA_COUNT' && // skip counter
+      i !== 'frameMetadataToString' // skip method
+      ) {
+        assert.doesNotThrow(() => { // jshint ignore:line
+          frame.frameMetadata(rs2.frame_metadata[i]);
+        });
+        assert.equal(Object.prototype.toString.call(
+          frame.frameMetadata(rs2.frame_metadata[i])
+        ), '[object Uint8Array]');
+      }
+    }
+  });
+
+  it('Testing method getData - 0 argument', () => {
+    assert.doesNotThrow(() => {
+      frame.getData();
+    });
+    assert(
+      Object.prototype.toString.call(frame.getData()), '[object Uint16Array]' ||
+      Object.prototype.toString.call(frame.getData()), '[object Uint8Array]' ||
+      Object.prototype.toString.call(frame.getData()), '[object Buffer]'
+    );
+  });
+
+  it('Testing method getData - buffer argument', () => {
+    const len = frame.dataByteLength;
+    let buf = new ArrayBuffer(len);
+    assert.doesNotThrow(() => {
+      frame.getData(buf);
+    });
+    assert(
+      Object.prototype.toString.call(buf), '[object Uint16Array]' ||
+      Object.prototype.toString.call(buf), '[object Uint8Array]' ||
+      Object.prototype.toString.call(buf), '[object Buffer]'
+    );
+  });
+
+  it('Testing method getData - 2 argument', () => {
+    assert.throws(() => {
+      frame.getData(1, 2);
+    });
+  });
+
+  it('Testing method getData - invalid argument', () => {
+    assert.throws(() => {
+      frame.getData('dummy');
+    });
+  });
+
+  it('Testing method supportsFrameMetadata - invalid argument', () => {
+    assert.throws(() => {
+      frame.supportsFrameMetadata('dummy');
+    });
+  });
+
+  it('Testing method supportsFrameMetadata - valid argument', () => {
+    for (let i in rs2.frame_metadata) {
+      if (rs2.frame_metadata[i] &&
+      i.toUpperCase() !== 'FRAME_METADATA_COUNT' && // skip counter
+      i !== 'frameMetadataToString' // skip method
+      ) {
+        assert.doesNotThrow(() => { // jshint ignore:line
+          frame.supportsFrameMetadata(rs2.frame_metadata[i]);
+        });
+        assert.equal(Object.prototype.toString.call(
+          frame.supportsFrameMetadata(rs2.frame_metadata[i])
+        ), '[object Boolean]');
+      }
+    }
+  });
+
+  it('Testing method destroy', () => {
+    assert.doesNotThrow(() => {
+      frame.destroy();
+    });
+  });
 });
