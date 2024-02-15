71fb7bcc62ff4eb9a3789a5f59704af1ae8a030d
Uraimov Botirjon <uraimovbotirjon@Uraimovs-MacBook-Pro.local>
2024-02-15 15:13:27 +0900

    develop for fitness measurement

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const { join } = require("path");
-const { spawn } = require('child_process');
-const net = require('net');
-
+const { spawn } = require("child_process");
+const net = require("net");
 
 let mainWindow;
 let pythonProcess = null;
 let client = null;
-
 
 function killPythonProcess() {
   if (client) {
@@ -20,7 +18,6 @@ function killPythonProcess() {
     pythonProcess = null;
   }
 }
-
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -36,84 +33,70 @@ function createWindow() {
   mainWindow.loadFile(join(__dirname, "index.html"));
   mainWindow.webContents.openDevTools();
 
-
   mainWindow.on("closed", () => {
     //client.destroy(); // Close the TCP connection
-  
+
     mainWindow = null;
   });
 
   // const pythonPath = 'C:\\Users\\TEST-USER\\.pyenv\\pyenv-win\\versions\\3.8.10\\python.exe'; // Correct Python path
   // const pythonScriptPath = join(__dirname, 'jumping.py');
   // pythonProcess = spawn(pythonPath, [pythonScriptPath]);
-  
+
   let retryInterval = 1000; // Retry every 2 seconds
   let maxRetries = 5;
   let retries = 0;
-  
-    // Setup TCP client connection
-    function connectClient() {
-  
+
+  // Setup TCP client connection
+  function connectClient() {
     client = new net.Socket();
-    client.connect(5002, '127.0.0.1', () => {
-      console.log('Connected to Python server');
-      mainWindow.webContents.send('server-status', 'connected');
+    client.connect(5004, "0.0.0.0", () => {
+      console.log("Connected to Python server");
+      mainWindow.webContents.send("server-status", "connected");
       retries = 0; // Reset retry count on successful connection
     });
-  
-    client.on('error', (err) => {
-      console.error('Socket error:', err);
+
+    client.on("error", (err) => {
+      console.error("Socket error:", err);
       if (retries < maxRetries) {
         setTimeout(connectClient, retryInterval);
         retries++;
       } else {
-        mainWindow.webContents.send('server-status', 'failed');
+        mainWindow.webContents.send("server-status", "failed");
       }
     });
-  
-    let buffer = '';
-  
-    client.on('data', function(dataBuffer) {
+
+    let buffer = "";
+
+    client.on("data", function (dataBuffer) {
       buffer += dataBuffer.toString();
-      let messages = buffer.split('\n'); // Split using the delimiter
+      let messages = buffer.split("\n"); // Split using the delimiter
       buffer = messages.pop();
-      
+
       messages.forEach((message) => {
-          if (message) {
-              try {
-                  const frameData = JSON.parse(message);
-                  mainWindow.webContents.send('frame-data', frameData);
-              } catch (err) {
-                  console.error('Error parsing data:', err.message);
-              }
+        if (message) {
+          try {
+            const frameData = JSON.parse(message);
+            mainWindow.webContents.send("frame-data", frameData);
+          } catch (err) {
+            console.error("Error parsing data:", err.message);
           }
+        }
       });
-  });
-    
-  
-    client.on('close', () => {
-      console.log('Connection closed');
-      mainWindow.webContents.send('server-status', 'disconnected');
+    });
+
+    client.on("close", () => {
+      console.log("Connection closed");
+      mainWindow.webContents.send("server-status", "disconnected");
     });
   }
-  
-  
+
   connectClient(); // Initial attempt to connect
-  
-  
-
-
 }
-
-
 
 app.on("ready", createWindow);
 
-
-
-
-
-ipcMain.on('save-command', (event, command) => {
+ipcMain.on("save-command", (event, command) => {
   if (client) {
     client.write(command);
   }
@@ -124,5 +107,5 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  if (mainWindow === null) createWindow
+  if (mainWindow === null) createWindow;
 });
